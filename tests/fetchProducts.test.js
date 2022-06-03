5b82adb28d7079b8508d4c2af975dddb56ae4fd2
Matheus Santos <matheus.p.1996@hotmail.com>
2022-06-03 16:08:55 -0300

    Resolução requisito 1 (corrigido)

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,11 +4,12 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   it('Deve ser uma função', async () => {
+
     expect(typeof fetchProducts).toBe('function');
   });
   it('Com o argumento "computador", a função fetch deve ser chamada', async () => {
-    await fetchProducts('computador');
-
+    const teste = await fetchProducts('computador');
+    
     expect(fetch).toHaveBeenCalled();
   });
   it('Com o argumento "computador", a função fetch deve ser chamada com o endpoint correto', async () => {
@@ -18,12 +19,12 @@ describe('1 - Teste a função fetchProducts', () => {
     expect(fetch).toHaveBeenCalledWith(endpoint);
   });
   it('Deve retornar um objeto com as propriedades esperadas', async () => {
-    const response = await getMagicCard('computador');
+    const response = await fetchProducts('computador');
 
     expect(response).toEqual(computadorSearch);
   });
   it('Sem argumento, deve retornar um erro com a mensagem "You must provide an url"', async () => {
-    const response = await getMagicCard();
+    const response = await fetchProducts();
 
     expect(response).toEqual(new Error('You must provide an url'));
   });
