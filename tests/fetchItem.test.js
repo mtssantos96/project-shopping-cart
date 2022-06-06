97af120e58d6479d69aa144180ac59e95499ca18
Matheus Santos <matheus.p.1996@hotmail.com>
2022-06-06 15:31:26 -0300

    Resolução requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,29 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Deve ser uma função', async () => {
+
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Com o argumento "MLB1615760527", a função fetch deve ser chamada', async () => {
+    const teste = await fetchItem('MLB1615760527');
+    
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Com o argumento "MLB1615760527", a função fetch deve ser chamada com o endpoint correto', async () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem('MLB1615760527');
+
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('Deve retornar um objeto com as propriedades esperadas', async () => {
+    const response = await fetchItem('MLB1615760527');
+
+    expect(response).toEqual(item);
+  });
+  it('Sem argumento, deve retornar um erro com a mensagem "You must provide an url"', async () => {
+    const response = await fetchItem();
+
+    expect(response).toEqual(new Error('You must provide an url'));
+  });
 });
